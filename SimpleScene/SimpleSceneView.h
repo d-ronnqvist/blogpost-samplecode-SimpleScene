a87d6393c3d4f3ec407265241c19af4bbf24d2ad
David Rönnqvist <david.ronnqvist@gmail.com>
2013-05-05 20:06:09 +0200

    Added a view for the Simple Scene and configured it to be the main view. Added imported frameworks to the Scene View

---
+++ SimpleScene/SimpleSceneView.h
@@ -0,0 +1,15 @@
+//
+//  SimpleSceneView.h
+//  SimpleScene
+//
+//  Created by David Rönnqvist on 5/5/13.
+//  Copyright (c) 2013 David Rönnqvist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <SceneKit/SceneKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface SimpleSceneView : SCNView
+
+@end
---
+++ SimpleScene/SimpleSceneView.m
@@ -0,0 +1,28 @@
+//
+//  SimpleSceneView.m
+//  SimpleScene
+//
+//  Created by David Rönnqvist on 5/5/13.
+//  Copyright (c) 2013 David Rönnqvist. All rights reserved.
+//
+
+#import "SimpleSceneView.h"
+
+@implementation SimpleSceneView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
